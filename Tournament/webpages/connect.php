8e6ac3ab429c3de526c75144986c3f6299101021
Ajay Sharma <94903858+AjaySharma2003@users.noreply.github.com>
2023-03-16 20:47:20 +0530

    Delete Tournament directory

--- Tournament/webpages/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost","root","root","tournament");
-if(!$conn)
-{
-    die(mysqli_connect_error());
-}
-?>
--- Tournament/webpages/time.php
+++
@@ -1,8 +0,0 @@
-<?php
-date_default_timezone_set('Asia/Kolkata');
-$today = date('Y-m-d');
-$today_str = strtotime($today);
-$from = date('Y-m-d',strtotime("+7 day", $today_str));
-$valid_date = date('Y-m-d',strtotime("+2 month", $today_str));
-$time_now = date('H:i:s');
-?>
